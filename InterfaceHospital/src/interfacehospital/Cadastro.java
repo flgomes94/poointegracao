53f2b2b467da737405b8b42c66a9cc993f6b6b44
Unknown <samuelreboucas07@hotmail.com>
2017-02-11 14:47:56 -0300

    Interface do trabalho, ainda serão feitas modificações, fiquem a vondade para dar sugestões

---
+++ InterfaceHospital/src/interfacehospital/Cadastro.java
@@ -0,0 +1,202 @@
+
+package interfacehospital;
+
+import java.awt.Insets;
+import javax.swing.border.EmptyBorder;
+
+/**
+ *
+ * @author Samuel R
+ */
+public class Cadastro extends javax.swing.JInternalFrame {
+
+    /**
+     * Creates new form Cadastro
+     */
+    public Cadastro() {
+        ((javax.swing.plaf.basic.BasicInternalFrameUI) this.getUI()).setNorthPane(null); 
+        initComponents();
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        painelEscolhaCadastro = new javax.swing.JPanel();
+        jComboCadastro = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        PainelCad = new javax.swing.JPanel();
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        painelEscolhaCadastro.setBackground(new java.awt.Color(0, 102, 102));
+
+        jComboCadastro.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Administrador", "Enfermeiro", "Fornecedor", "Maqueiro", "Serviços Gerais", "Vigilante" }));
+        jComboCadastro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboCadastroActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Tw Cen MT", 1, 18)); // NOI18N
+        jLabel2.setText("Escolha sua opção de cadastro:");
+
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT", 1, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Cadastro");
+
+        javax.swing.GroupLayout PainelCadLayout = new javax.swing.GroupLayout(PainelCad);
+        PainelCad.setLayout(PainelCadLayout);
+        PainelCadLayout.setHorizontalGroup(
+            PainelCadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        PainelCadLayout.setVerticalGroup(
+            PainelCadLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 404, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout painelEscolhaCadastroLayout = new javax.swing.GroupLayout(painelEscolhaCadastro);
+        painelEscolhaCadastro.setLayout(painelEscolhaCadastroLayout);
+        painelEscolhaCadastroLayout.setHorizontalGroup(
+            painelEscolhaCadastroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painelEscolhaCadastroLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(32, 32, 32)
+                .addComponent(jComboCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(painelEscolhaCadastroLayout.createSequentialGroup()
+                .addGap(292, 292, 292)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 157, Short.MAX_VALUE)
+                .addGap(294, 294, 294))
+            .addComponent(PainelCad, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        painelEscolhaCadastroLayout.setVerticalGroup(
+            painelEscolhaCadastroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painelEscolhaCadastroLayout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(painelEscolhaCadastroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jComboCadastro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(PainelCad, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jScrollPane1.setViewportView(painelEscolhaCadastro);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jComboCadastroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboCadastroActionPerformed
+        PainelCad.removeAll();
+        if(this.jComboCadastro.getSelectedItem().equals("Administrador"))
+        {
+            CadastroGeral CadGeral = new CadastroGeral();
+            CadGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelCad.add(CadGeral);
+                CadGeral.setMaximum(true);
+                CadGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+        if(this.jComboCadastro.getSelectedItem() == "Enfermeiro")
+        {
+            
+            CadastroEnfermeiro CadEnfermeiros = new CadastroEnfermeiro();
+            CadEnfermeiros.setBorder(new EmptyBorder(new Insets(0,0,0,0)));       
+            try {
+                PainelCad.add(CadEnfermeiros);
+                CadEnfermeiros.setMaximum(true);
+                CadEnfermeiros.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+
+        if(this.jComboCadastro.getSelectedItem() == "Fornecedor")
+        {
+            CadastroGeral CadGeral = new CadastroGeral();
+            CadGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelCad.add(CadGeral);
+                CadGeral.setMaximum(true);
+                CadGeral.setVisible(true);
+            } catch (Exception e) {
+            }    
+        }
+        
+        
+        
+        if(this.jComboCadastro.getSelectedItem() == "Maqueiro")
+        {
+            CadastroGeral CadGeral = new CadastroGeral();
+            CadGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelCad.add(CadGeral);
+                CadGeral.setMaximum(true);
+                CadGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+        
+        
+        if(this.jComboCadastro.getSelectedItem() == "Serviços Gerais")
+        {
+            CadastroGeral CadGeral = new CadastroGeral();
+            CadGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelCad.add(CadGeral);
+                CadGeral.setMaximum(true);
+                CadGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }    
+            
+        if(this.jComboCadastro.getSelectedItem() == "Vigilante")
+        {
+         CadastroGeral CadGeral = new CadastroGeral();
+            CadGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelCad.add(CadGeral);
+                CadGeral.setMaximum(true);
+                CadGeral.setVisible(true);
+            } catch (Exception e) {
+            }    
+        }
+    }//GEN-LAST:event_jComboCadastroActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PainelCad;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> jComboCadastro;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JPanel painelCadastro;
+    private javax.swing.JPanel painelCadastro1;
+    private javax.swing.JPanel painelEscolhaCadastro;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ InterfaceHospital/src/interfacehospital/Consulta.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacehospital;
+
+import java.awt.Insets;
+import javax.swing.border.EmptyBorder;
+
+/**
+ *
+ * @author Samuel R
+ */
+public class Consulta extends javax.swing.JInternalFrame {
+
+    public Consulta() {
+         ((javax.swing.plaf.basic.BasicInternalFrameUI) this.getUI()).setNorthPane(null); 
+        initComponents();
+    }   
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        painelEscolhaConsulta = new javax.swing.JPanel();
+        jComboConsulta = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        PainelConsulta = new javax.swing.JPanel();
+
+        painelEscolhaConsulta.setBackground(new java.awt.Color(0, 102, 102));
+
+        jComboConsulta.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Administrador", "Enfermeiro", "Fornecedor", "Maqueiro", "Serviços Gerais", "Vigilante" }));
+        jComboConsulta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboConsultaActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Tw Cen MT", 1, 18)); // NOI18N
+        jLabel2.setText("Escolha sua opção de consulta:");
+
+        jLabel1.setBackground(new java.awt.Color(0, 102, 102));
+        jLabel1.setFont(new java.awt.Font("Tw Cen MT", 1, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Consulta");
+
+        javax.swing.GroupLayout painelEscolhaConsultaLayout = new javax.swing.GroupLayout(painelEscolhaConsulta);
+        painelEscolhaConsulta.setLayout(painelEscolhaConsultaLayout);
+        painelEscolhaConsultaLayout.setHorizontalGroup(
+            painelEscolhaConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(painelEscolhaConsultaLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(jComboConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painelEscolhaConsultaLayout.createSequentialGroup()
+                .addGap(236, 236, 236)
+                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(212, 212, 212))
+        );
+        painelEscolhaConsultaLayout.setVerticalGroup(
+            painelEscolhaConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, painelEscolhaConsultaLayout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(painelEscolhaConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jComboConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(37, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout PainelConsultaLayout = new javax.swing.GroupLayout(PainelConsulta);
+        PainelConsulta.setLayout(PainelConsultaLayout);
+        PainelConsultaLayout.setHorizontalGroup(
+            PainelConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        PainelConsultaLayout.setVerticalGroup(
+            PainelConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 325, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(PainelConsulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(painelEscolhaConsulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, 0))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(painelEscolhaConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(10, 10, 10)
+                .addComponent(PainelConsulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jComboConsultaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboConsultaActionPerformed
+        PainelConsulta.removeAll();
+        if(this.jComboConsulta.getSelectedItem().equals("Administrador"))
+        {
+            ConsultaGeral ConsGeral = new ConsultaGeral();
+            ConsGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelConsulta.add(ConsGeral);
+                ConsGeral.setMaximum(true);
+                ConsGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+        if(this.jComboConsulta.getSelectedItem().equals("Enfermeiro"))
+        {
+            ConsultaEnfermeiro ConsEnfermeiro = new ConsultaEnfermeiro();
+            ConsEnfermeiro.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelConsulta.add(ConsEnfermeiro);
+                ConsEnfermeiro.setMaximum(true);
+                ConsEnfermeiro.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+
+        if(this.jComboConsulta.getSelectedItem().equals("Fornecedor"))
+        {
+            ConsultaGeral ConsGeral = new ConsultaGeral();
+            ConsGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelConsulta.add(ConsGeral);
+                ConsGeral.setMaximum(true);
+                ConsGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+        
+          
+        if(this.jComboConsulta.getSelectedItem().equals("Maqueiro"))
+        {
+            ConsultaGeral ConsGeral = new ConsultaGeral();
+            ConsGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelConsulta.add(ConsGeral);
+                ConsGeral.setMaximum(true);
+                ConsGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+
+        }
+        
+        
+        if(this.jComboConsulta.getSelectedItem().equals("Serviços Gerais"))
+        {
+            ConsultaGeral ConsGeral = new ConsultaGeral();
+            ConsGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelConsulta.add(ConsGeral);
+                ConsGeral.setMaximum(true);
+                ConsGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+            
+        if(this.jComboConsulta.getSelectedItem().equals("Vigilante"))
+        {
+            ConsultaGeral ConsGeral = new ConsultaGeral();
+            ConsGeral.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
+       
+            try {
+                PainelConsulta.add(ConsGeral);
+                ConsGeral.setMaximum(true);
+                ConsGeral.setVisible(true);
+            } catch (Exception e) {
+            }
+        }
+    }//GEN-LAST:event_jComboConsultaActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PainelConsulta;
+    private javax.swing.JComboBox<String> jComboConsulta;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel painelEscolhaConsulta;
+    // End of variables declaration//GEN-END:variables
+}
