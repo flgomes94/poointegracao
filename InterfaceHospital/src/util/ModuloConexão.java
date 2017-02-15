7ec8825595f2d2ef6c78a5319706e1860a26b16c
FVjesus <Micro@Micro-PC>
2017-02-15 20:13:00 -0200

    Classe fornecedor Classe moduloConexão Classe Dao adicionadas

---
+++ InterfaceHospital/src/util/ModuloConexão.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Micro
+ */
+public class ModuloConexão {
+    public static Connection conector(){
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/funcionario";
+        String user = "root";
+        String password = "vertrigo";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro na conexão com o banco de dados:\n" + e);
+            return null;
+        }
+    
+}
+}
