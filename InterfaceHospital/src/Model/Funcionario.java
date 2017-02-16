117e18fbc2cdd93e279d9885e667233f3997c315
Lucas Souza <lucas252souza@gmail.com>
2017-02-15 22:05:28 -0300

    ajustando pack

---
+++ InterfaceHospital/src/Model/Funcionario.java
@@ -0,0 +1,87 @@
+
+package Model;
+
+public class Funcionario {    
+   String nome;
+   String cpf;
+   String rg;
+   String telefone;
+   String cargo;
+   String salario;
+   String endereco;
+   String cidade;
+
+    public Funcionario() {
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" + "nome=" + nome + ", cpf=" + cpf + ", rg=" + rg + ", telefone=" + telefone + ", cargo=" + cargo + ", salario=" + salario + ", endereco=" + endereco + ", cidade=" + cidade + '}';
+    }
+   
+   
+}
